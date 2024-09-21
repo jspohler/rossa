c19206387523c128e37e9fedd21fd85ba709020f
oscar junior tsakam <88436529+digtoj@users.noreply.github.com>
2024-09-21 07:40:22 -0700

    Update main.py

--- chatbot/main.py
+++ chatbot/main.py
@@ -6,7 +6,19 @@ from langchain_community.utilities import SQLDatabase
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 import streamlit as st
+import os
 
+
+# Access environment variables
+database_host = os.getenv('DATABASE_HOST')
+database_user = os.getenv('DATABASE_USER')
+database_password = os.getenv('DATABASE_PASSWORD')
+database_name = os.getenv('DATABASE_NAME')
+database_port = os.getenv('DATABASE_PORT')
+
+user_normal = "Normal"
+user_admin = "Admin"
+grants = [user_normal, user_admin]
 
 def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
   db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
@@ -51,22 +63,26 @@ def get_response(user_query: str, db: SQLDatabase, chat_history: list):
   sql_chain = get_sql_chain(db)
   
   template = """
-    You work at a help desk at a company. You get requests from a coworker that is asking you questions about which person is responsible for their specified use case so they can contact and connect with them.
-    Based on the table schema below, question, sql query, and sql response, write a natural language response in german and format the contact information like this: 
-    Name: \n
-    Abteilung: \n
-    Position: \n
-    Emailaddresse: \n
-    Telefonnummer: \n
-    Standort: \n
-    Betruete Programme: \n
+     You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
     
     <SCHEMA>{schema}</SCHEMA>
-
+    
     Conversation History: {chat_history}
-    SQL Query: <SQL>{query}</SQL>
-    User question: {question}
-    SQL Response: {response}"""
+    
+    Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+    
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
+    
+    Your turn:
+    
+    Question: {question}
+    SQL Query:
+    """
   
   prompt = ChatPromptTemplate.from_template(template)
   
@@ -94,33 +110,14 @@ if "chat_history" not in st.session_state:
     ]
 
 load_dotenv()
+db = init_database(database_user, database_password, database_host, database_port, database_name)
+
+st.session_state.db = db
 
 st.set_page_config(page_title="Chat with Rossa", page_icon=":rossman_icon_Evl_2.ico:")
 
 st.title("Chat with Rossa")
 
-with st.sidebar:
-    st.subheader("Settings")
-    st.write("")
-    
-    st.text_input("Host", value="localhost", key="Host")
-    st.text_input("Port", value="3306", key="Port")
-    st.text_input("User", value="rossa_user", key="User")
-    st.text_input("Password", type="password", value="rossa", key="Password")
-    st.text_input("Database", value="rossa_db", key="Database")
-    
-    if st.button("Connect"):
-        with st.spinner("Connecting to database..."):
-            db = init_database(
-                st.session_state["User"],
-                st.session_state["Password"],
-                st.session_state["Host"],
-                st.session_state["Port"],
-                st.session_state["Database"]
-            )
-            st.session_state.db = db
-            st.success("Connected to database!")
-    
 for message in st.session_state.chat_history:
     if isinstance(message, AIMessage):
         with st.chat_message("AI"):
@@ -129,7 +126,7 @@ for message in st.session_state.chat_history:
         with st.chat_message("Human"):
             st.markdown(message.content)
 
-user_query = st.chat_input("Who's ROSSponsible ?")
+user_query = st.chat_input("Was willst du wissen über 😎 ?")
 if user_query is not None and user_query.strip() != "":
     st.session_state.chat_history.append(HumanMessage(content=user_query))
     
@@ -137,6 +134,7 @@ if user_query is not None and user_query.strip() != "":
         st.markdown(user_query)
         
     with st.chat_message("AI"):
+      
         response = get_response(user_query, st.session_state.db, st.session_state.chat_history)
         st.markdown(response)
         
